7e0da9d3f090e19cadc489e0940247586a392c73
thienjs <thien.tran6@gmail.com>
2021-11-16 13:46:14 -0500

    first commit

---
+++ pages/about.js
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>This is an app to find pickup tennis matches.</p>
+        </div>
+    )
+}
---
+++ pages/events/[slug].js
@@ -0,0 +1,9 @@
+import { useRouter } from "next/router"
+
+export default function EventsPage() {
+    return (
+        <div>
+            <h1>Events</h1>
+        </div>
+    )
+}
